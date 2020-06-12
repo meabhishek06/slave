7d437a461bea127fc99e7d1c12042c112e389287
Abhishek <meabhishekarora06@gmail.com>
2020-06-12 23:07:38 +0530

    csdv

--- ku.sh
+++ ku.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+cd /root/slave1/
 sudo yum install curl 
 
 sudo curl -LO https://storage.googleapis.com/kubernetes-release/release/`curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt`/bin/linux/amd64/kubectl
